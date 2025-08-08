508683424f6174e68ba120c1ae4f12a2fd7fc0da
luisalealr <luisafernandalero@gmail.com>
2025-08-08 16:46:14 -0500

    feat: add Fonts component and styles for font selection

--- src/Pages/CreateNotes.jsx
+++ src/Pages/CreateNotes.jsx
@@ -1,9 +1,12 @@
 import { Colors } from "../Components/Notes/Colors"
+import { Fonts } from "../Components/Notes/Fonts"
+import style from "../Styles/notes.module.css"
 
 export const CreateNotes = () => {
   return (
-    <div>
+    <div className={style.notes_container}>
       <Colors />
+      <Fonts />
     </div>
   )
 }
