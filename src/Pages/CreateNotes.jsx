6cde8a8ddcf70fd8786095963442049a7eacd6a2
Andersson Camilo Cardenas Guarin <anderssonccg@gmail.com>
2025-08-11 11:24:34 -0500

    fix: resolve NoteForm import in CreateNotes page

--- src/Pages/CreateNotes.jsx
+++ src/Pages/CreateNotes.jsx
@@ -1,14 +1,14 @@
-
-import { Colors } from "../Components/Notes/Colors"
-import { Fonts } from "../Components/Notes/Fonts"
-import style from "../Styles/notes.module.css"
+import { NoteForm } from "../Components/Notes/NoteForm";
+import { Colors } from "../Components/Notes/Colors";
+import { Fonts } from "../Components/Notes/Fonts";
+import style from "../Styles/notes.module.css";
 
 export const CreateNotes = () => {
   return (
     <div className={style.notes_container}>
       <Colors />
-      <Fonts />
       <NoteForm />
+      <Fonts />
     </div>
   );
 };
