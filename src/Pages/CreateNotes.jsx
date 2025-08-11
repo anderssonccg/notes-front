975cd80d29b4fbe08200cf9db1581346228987c4
Andersson Camilo Cardenas Guarin <anderssonccg@gmail.com>
2025-08-11 10:51:11 -0500

    feat: add form to create notes

--- src/Pages/CreateNotes.jsx
+++ src/Pages/CreateNotes.jsx
@@ -1,9 +1,11 @@
-import { Colors } from "../Components/Notes/Colors"
+import { Colors } from "../Components/Notes/Colors";
+import { NoteForm } from "../Components/Notes/NoteForm";
 
 export const CreateNotes = () => {
   return (
     <div>
       <Colors />
+      <NoteForm />
     </div>
-  )
-}
+  );
+};
