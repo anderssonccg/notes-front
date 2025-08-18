7a7c8d28789679a3773e15af14ab6eda084bdae8
JosuePerez4 <josuedanielpg@ufps.edu.co>
2025-08-18 12:41:14 -0500

    Feat: Added button to delete multiple notes (#13)
    * Fix: Note and noteList now works better
    
    * Fix: Temporary add button removed
    
    * Fix: note and note list now works better with new changes
    
    * Feat: Added the functionality to edit notes
    
    * Feat: Added delete multiple notes

---
+++ src/Components/NoteList/TrashButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FcEmptyTrash, FcFullTrash } from "react-icons/fc";
+import style from "./TrashButton.module.css";
+export const TrashButton = ({ onDelete, notesToDelete }) => {
+  const hasSelection = notesToDelete.length > 0;
+
+  return (
+    <div>
+      <button
+        className={style.floating_delete_btn}
+        disabled={!hasSelection}
+        onClick={() => onDelete(notesToDelete.map((n) => n.id))}
+      >
+        {hasSelection ? <FcFullTrash /> : <FcEmptyTrash />}
+      </button>
+    </div>
+  );
+};
